bba9dbdccb8dac90fa7db4c86e2399dbcaf78429
David Freitas <davidcnt@outlook.com>
2023-01-13 15:57:52 -0300

    Adicionando descrição

--- bola.py
+++ bola.py
@@ -1,3 +1,11 @@
+"""
+Classe Bola: Crie uma classe que modele uma bola:
+
+Atributos: Cor, circunferência, material
+Métodos: trocaCor e mostraCor
+"""
+
+
 class Bola:
     def __init__(self, cor: str, circunferencia: int, material: str):
         self.cor = cor
--- quadrado.py
+++ quadrado.py
@@ -1,3 +1,9 @@
+"""
+Classe Quadrado: Crie uma classe que modele um quadrado:
+
+Atributos: Tamanho do lado
+Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+"""
 from time import sleep
 
 
