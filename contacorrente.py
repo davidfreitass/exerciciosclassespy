2dd6c49586657f1f7524ddb99f7c0dce53ca3cfc
David Freitas <davidcnt@outlook.com>
2023-01-17 16:54:34 -0300

    Classe ContaCorrente

--- contacorrente.py
+++ contacorrente.py
@@ -7,7 +7,7 @@ Métodos: alterarNome, depósito e saque; No construtor, saldo é opcional, com 
 
 
 class ContaCorrente:
-    def __init__(self, conta, nome, saldo):
+    def __init__(self, conta, nome, saldo=0):
         self.conta = conta
         self.nome = nome
         self.saldo = saldo
@@ -16,7 +16,27 @@ class ContaCorrente:
         self.nome = nome
 
     def depositar(self, dep):
-        return self.saldo + dep
+        self.saldo += dep
+        return self.saldo
 
     def sacar(self, sac):
-        return self.saldo - sac
+        self.saldo -= sac
+        return self.saldo
+
+    def __str__(self) -> str:
+        return (
+            "======================================\n"
+            f"| Número da Conta: {c.conta}\n"
+            f"| Nome do Correntista: {c.nome}\n"
+            f"| Saldo: R${c.saldo},00"
+        )
+
+
+c = ContaCorrente(12345, "David")
+print(c)
+c.alterarNome("David Freitas")
+print(c)
+c.depositar(3800)
+print(c)
+c.sacar(425)
+print(c)
